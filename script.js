13a14ab36ad205ec6f140ea63a29b181c3c935b3
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-03-07 12:40:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// script.js
+let element = document.getElementById("level");
+let level = 0;
+while (element) {
+  level++;
+  element = element.parentElement;
+}
+alert("The level of the element is: " + level);
